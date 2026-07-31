ba238d337cdf7a47fa5303cbb4a766da8d46438c
talupulasekhar <69070724+talupulasekhar@users.noreply.github.com>
2026-07-31 09:51:07 +0530

    Update test_oone.py

--- packages/organization/tests/test_oone.py
+++ packages/organization/tests/test_oone.py
@@ -1,7 +1,7 @@
 import unittest
 # Import the functions you want to test
 #from organization.oone import printPolo
-from packages.organizationsrc.organization.oone import printPolo
+from packages.organization.src.organization.oone import printPolo
 
 class test_print_polo(unittest.TestCase):
     def test_print_hello(self):
